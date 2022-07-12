23dcd47355c3e527718093ddab26cdbad295d9e9
Vika Globa <v.globa@dunice.net>
2022-07-12 12:02:59 +0300

    Fixed saga

--- src/redux/sagas/loginSaga.js
+++ src/redux/sagas/loginSaga.js
@@ -3,13 +3,12 @@ import { takeLatest, put, call } from 'redux-saga/effects';
 import { loginError, loginSuccess } from '../actions/actionCreators/login';
 import { loginTypes } from '../actions/types';
 import api from '../../api';
-import { saveToken, getToken } from '../../helpers/localStorage';
+import { saveToken } from '../../helpers/localStorage';
 
 function* loginWorker({ payload }) {
   try {
     const { data } = yield call(api.post, '/authorization', payload);
     saveToken(data.token);
-    Boolean(getToken());
     yield put(loginSuccess(data.data));
   } catch (error) {
     yield put(loginError(error.message));
